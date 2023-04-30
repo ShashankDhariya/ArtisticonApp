c73e911b5ea52a50a98a7239e418b72bd6a91eae
Yash <yashpundir2001@gmail.com>
2023-05-01 03:46:27 +0530

    home page changes

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,9 @@
+import 'package:artist_icon/screens/signIn.dart';
+import 'package:artist_icon/screens/signup.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:line_icons/line_icons.dart';
 import 'artistpage.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,33 +14,64 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  List<Widget> _widgetOptions = [Artistpage(), SignUpPage(), SignInPage()];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Color(0xff2dbdc0),
-        unselectedItemColor: Color(0xffaab2ca),
-        showUnselectedLabels: false,
-        showSelectedLabels: false,
-        type: BottomNavigationBarType.fixed,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Padding(
-              padding: EdgeInsets.only(right: 36),
-              child: Icon(Icons.home),
-            ),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(icon: Icon(Icons.add_card), label: 'Add'),
-          BottomNavigationBarItem(
-              icon: Padding(
-                padding: EdgeInsets.only(left: 36),
-                child: Icon(Icons.person),
+      bottomNavigationBar: Container(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20),
+          child: GNav(
+            selectedIndex: _selectedIndex,
+            onTabChange: (index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+            backgroundColor: Colors.black,
+            color: Colors.white,
+            tabBackgroundColor: Colors.grey,
+            gap: 8,
+            padding: EdgeInsets.all(16),
+            tabs: [
+              GButton(
+                icon: LineIcons.home,
+                text: 'Home',
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Artistpage()),
+                  );
+                },
               ),
-              label: 'Profile')
-        ],
+              GButton(
+                icon: LineIcons.home,
+                text: 'Home',
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SignInPage()),
+                  );
+                },
+              ),
+              GButton(
+                icon: LineIcons.home,
+                text: 'Home',
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SignUpPage()),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
       ),
-      body: Artistpage(),
+      body: _widgetOptions[_selectedIndex],
     );
   }
 }
