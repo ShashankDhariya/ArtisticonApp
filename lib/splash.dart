6446ad7dc1fde92cd7f471af4c3aa32fbb33ae09
Shashank Dhariya <dhariyashashank@gmail.com>
2023-05-09 02:15:45 +0530

    UiChange

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,5 +1,4 @@
 import 'package:artist_icon/screens/land_screen.dart';
-import 'package:artist_icon/screens/user_details.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
@@ -23,7 +22,7 @@ class _SplashState extends State<Splash> {
       context, 
       MaterialPageRoute(
         builder:(context) {
-          return UserDetails();
+          return LandScreen();
         },
       )
     );
