2d90cd16f0f3e70caf4e04748b160e0ef89dbcc9
Shashank Dhariya <dhariyashashank@gmail.com>
2023-05-13 01:02:12 +0530

    App

--- lib/screens/profileOptions/edit.dart
+++ lib/screens/profileOptions/edit.dart
@@ -1,5 +1,4 @@
 import 'package:artist_icon/screens/components/my_text_field.dart';
-import 'package:artist_icon/screens/components/my_button.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
