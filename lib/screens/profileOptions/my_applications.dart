4e827c61b99a60fa2cf4affa10cc9ec5c4620075
Yash <yashpundir2001@gmail.com>
2023-05-22 18:49:11 +0530

    App name changes

--- lib/screens/profileOptions/my_applications.dart
+++ lib/screens/profileOptions/my_applications.dart
@@ -10,7 +10,9 @@ import 'package:artist_icon/screens/home.dart';
 class MyApplications extends StatelessWidget {
   final UserModel userModel;
   final User firebaseUser;
-  const MyApplications({Key? key, required this.userModel, required this.firebaseUser}): super(key: key);
+  const MyApplications(
+      {Key? key, required this.userModel, required this.firebaseUser})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +29,8 @@ class MyApplications extends StatelessWidget {
         foregroundColor: Colors.black,
         backgroundColor: Colors.grey.shade100,
         centerTitle: true,
-        title: Text('My Applications',
+        title: Text(
+          'My Applications',
           style: GoogleFonts.nunito(
             fontWeight: FontWeight.bold,
             color: Colors.black,
@@ -52,26 +55,37 @@ class MyApplications extends StatelessWidget {
             ],
           ),
           StreamBuilder(
-            stream: FirebaseFirestore.instance.collection("Users").doc(userModel.uid).collection("MyApplications").snapshots(),
+            stream: FirebaseFirestore.instance
+                .collection("Users")
+                .doc(userModel.uid)
+                .collection("MyApplications")
+                .snapshots(),
             builder: (context, snapshot) {
               if (snapshot.connectionState == ConnectionState.active) {
                 if (snapshot.hasData) {
                   QuerySnapshot data = snapshot.data as QuerySnapshot;
-                  if (data.docs.isEmpty) {  // Display a message and a button to redirect to the home page
+                  if (data.docs.isEmpty) {
+                    // Display a message and a button to redirect to the home page
                     return Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Text("You haven't applied to a job or availed a service",
-                          style: GoogleFonts.nunito(fontWeight: FontWeight.w700,fontSize: 16),
+                        Text(
+                          "You haven't applied to a job or availed a service",
+                          style: GoogleFonts.nunito(
+                              fontWeight: FontWeight.w700, fontSize: 16),
                         ),
-                        SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+                        SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.02),
                         SizedBox(
                           width: 200,
                           child: ElevatedButton(
                             onPressed: () {
                               Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => HomePage(userModel: userModel,firebaseUser: firebaseUser)));
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => HomePage(
+                                          userModel: userModel,
+                                          firebaseUser: firebaseUser)));
                             },
                             style: ElevatedButton.styleFrom(
                               padding: const EdgeInsets.symmetric(vertical: 16),
@@ -80,7 +94,8 @@ class MyApplications extends StatelessWidget {
                               ),
                               backgroundColor: Colors.teal.shade300,
                             ),
-                            child: Text('Apply or Rent Something',
+                            child: Text(
+                              'Apply or Rent Something',
                               style: GoogleFonts.nunito(
                                 textStyle: const TextStyle(
                                   color: Colors.white,
@@ -96,7 +111,9 @@ class MyApplications extends StatelessWidget {
                     return ListView.builder(
                       itemCount: data.docs.length,
                       itemBuilder: (context, index) {
-                        MyApplicationsModel currApplication = MyApplicationsModel.fromMap(data.docs[index].data() as Map<String, dynamic>);
+                        MyApplicationsModel currApplication =
+                            MyApplicationsModel.fromMap(data.docs[index].data()
+                                as Map<String, dynamic>);
                         return Padding(
                           padding: const EdgeInsets.symmetric(
                               horizontal: 16, vertical: 12),
@@ -111,37 +128,68 @@ class MyApplications extends StatelessWidget {
                                 Container(
                                   decoration: BoxDecoration(
                                     image: const DecorationImage(
-                                      image: AssetImage("assets/images/job_tile_background3.jpg"),
+                                      image: AssetImage(
+                                          "assets/images/job_tile_background3.jpg"),
                                       fit: BoxFit.cover,
                                     ),
                                     borderRadius: BorderRadius.circular(20),
                                   ),
                                 ),
                                 ListTile(
-                                  leading: Text(currApplication.type.toString()),
+                                  leading: Text(currApplication.type.toString(),
+                                      style: GoogleFonts.nunito(
+                                          fontWeight: FontWeight.w700)),
                                   trailing: CupertinoButton(
-                                    onPressed: () {  
-                                      FirebaseFirestore.instance.collection("Users").doc(userModel.uid.toString()).collection("MyApplications").doc(currApplication.id.toString()).delete();
-                                      FirebaseFirestore.instance.collection('${currApplication.type}s').doc(currApplication.id).collection('Applications').doc(userModel.uid.toString()).delete();
+                                    onPressed: () {
+                                      FirebaseFirestore.instance
+                                          .collection("Users")
+                                          .doc(userModel.uid.toString())
+                                          .collection("MyApplications")
+                                          .doc(currApplication.id.toString())
+                                          .delete();
+                                      FirebaseFirestore.instance
+                                          .collection(
+                                              '${currApplication.type}s')
+                                          .doc(currApplication.id)
+                                          .collection('Applications')
+                                          .doc(userModel.uid.toString())
+                                          .delete();
                                     },
-                                    child: const Text("Delete")
-                                    ), 
-                                  title: Text(currApplication.name.toString(),
-                                    style: const TextStyle(
-                                      fontWeight: FontWeight.w600, fontSize: 20),
+                                    child: Text("Delete",
+                                        style: GoogleFonts.nunito(
+                                            fontWeight: FontWeight.w700,
+                                            color: Colors.blue.shade700)),
+                                  ),
+                                  title: Text(
+                                    currApplication.name.toString(),
+                                    style: GoogleFonts.nunito(
+                                        fontWeight: FontWeight.w700),
                                   ),
                                   subtitle: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
                                     children: [
-                                      Text(currApplication.category.toString(), style: const TextStyle(fontWeight: FontWeight.bold)),
-                                      const SizedBox(height: 10),
-                                      Text(currApplication.address.toString()),
-                                      const SizedBox(height: 10),
+                                      Text(currApplication.category.toString(),
+                                          style: GoogleFonts.nunito(
+                                              fontWeight: FontWeight.w600)),
+                                      const SizedBox(height: 7),
+                                      Text(currApplication.address.toString(),
+                                          style: GoogleFonts.nunito(
+                                              fontWeight: FontWeight.w500)),
+                                      const SizedBox(height: 7),
                                       Row(
-                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
                                         children: [
-                                          Text(currApplication.date.toString().substring(0, 10)),
-                                          Text('Rs.${currApplication.pay}'),
+                                          Text(
+                                              currApplication.date
+                                                  .toString()
+                                                  .substring(0, 10),
+                                              style: GoogleFonts.nunito(
+                                                  fontWeight: FontWeight.w500)),
+                                          Text('Rs.${currApplication.pay}',
+                                              style: GoogleFonts.nunito(
+                                                  fontWeight: FontWeight.w500)),
                                         ],
                                       ),
                                     ],
